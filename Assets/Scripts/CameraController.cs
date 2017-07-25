3a298f7aee2c3c0eea108a48e1fd9827952f513a
stefankreitmayer <stefankreitmayer@gmail.com>
2017-07-25 15:05:15 +0200

    Import project

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject tree;
+	private Camera cam;
+	private Rigidbody rigidBody;
+	private float speed = 0.5f;
+
+	void Start(){
+		cam = Camera.main;
+		rigidBody = cam.GetComponent<Rigidbody>();
+	}
+
+	void Update () {
+		Bounds bounds = GetBounds(tree.transform.GetChild(0));
+		foreach(Transform child in tree.transform){
+			bounds.Encapsulate(GetBounds(child));
+		}
+		float camDistance = - cam.transform.position.z;
+		if(camDistance < 70 &&
+			(IsOutOfView(new Vector3(bounds.min.x, bounds.max.y, bounds.min.z))
+				|| IsOutOfView(new Vector3(bounds.max.x, bounds.max.y, bounds.min.z)))){
+			rigidBody.AddForce(new Vector3(0, 0, - (2+camDistance) * speed));
+		}
+		Vector3 rotation = cam.transform.eulerAngles;
+		float rotX = Mathf.Max (-20, - Mathf.Sqrt ((camDistance+1) / 70f) * 10);
+		cam.transform.eulerAngles = new Vector3 (rotX, rotation.y, rotation.z);
+	}
+
+	private Bounds GetBounds(Transform t){
+		return t.gameObject.GetComponent<MeshRenderer> ().bounds;
+	}
+
+	private bool IsOutOfView(Vector3 point){
+		Plane[] planes = GeometryUtility.CalculateFrustumPlanes(cam);
+		return !(planes[0].GetSide(point) && planes[1].GetSide(point) && planes[3].GetSide(point));
+	}
+}
---
+++ Assets/Scripts/ClickplaneController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickplaneController : MonoBehaviour {
+	public GameObject tree;
+	public GameObject nodePrefab;
+	private GameObject offspring;
+
+	void OnMouseDown(){
+		SpawnOffspring ();
+	}   
+
+	void Update(){
+		if (Input.GetMouseButton (0)) {
+			offspring.transform.localScale *= 1 + Time.deltaTime;
+		}
+	}
+
+	private void SpawnOffspring(){
+		Camera camera = Camera.main;
+		Vector2 mousePos = Input.mousePosition;
+		Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+		RaycastHit hit;
+		if (Physics.Raycast(ray, out hit)){
+			Vector3 pos = FindNearestSilhouettePoint (hit.point);
+			offspring = Instantiate (nodePrefab);
+			offspring.transform.position = new Vector3(pos.x, pos.y, 0);
+			offspring.transform.localScale = Vector3.one * .25f;
+			offspring.transform.parent = tree.transform;
+		}
+	}
+
+	private Vector3 FindNearestSilhouettePoint(Vector3 userPoint){
+		Vector3 winner = Vector3.zero;
+		foreach (Transform child in tree.transform) {
+			Vector3 candidate = FindSilhouettePoint (child, userPoint);
+			if (Vector3.Distance (candidate, userPoint) < Vector3.Distance (winner, userPoint)) {
+				winner = candidate;
+			}
+		}
+		return winner;
+	}
+
+	private Vector3 FindSilhouettePoint(Transform transform, Vector3 userPoint){
+		Vector3 center = transform.position;
+		float dx = userPoint.x - center.x;
+		float dy = userPoint.y - center.y;
+		float angle = Mathf.Atan2(dy, dx);
+		float radius = transform.localScale.x / 2;
+		float x = center.x + Mathf.Cos (angle) * radius;
+		float y = center.y + Mathf.Sin (angle) * radius;
+		return new Vector3 (x, y, 0);
+	}
+}
